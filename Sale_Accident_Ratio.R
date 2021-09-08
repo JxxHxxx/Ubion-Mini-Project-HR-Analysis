6d36bec82001e6f59f7ee0b4ceaec97d20c68904
JxxH <87173870+JxxHxxx@users.noreply.github.com>
2021-09-08 20:07:56 +0900

    Add files via upload

--- Sale_Accident_Ratio.R
+++ Sale_Accident_Ratio.R
@@ -3,11 +3,12 @@ HR = raw_HR
 plot1 = raw_HR$satisfaction_level 
 
 acc = table(raw_HR$Work_accident)
+accident_ratio = acc[[2]]/(acc[[1]] + acc[[2]]) # 회사의 사고율
 
 p = ggplot(data = raw_HR, aes(raw_HR$Work_accident)) + geom_bar()
-p #  전체 직원 사고 유무
 
-class(acc)
+p#  전체 직원 사고 유무
+
 
 sales_accident = HR %>%
   group_by(sales) %>%
@@ -28,22 +29,11 @@ accident_pop_df = sort(accident_pop_df, decreasing = T)
 
 sales_accident_chart = ggplot(data = accident_pop_df, 
                       aes(x = reorder(sales, -(accident - accident_ratio)), y = accident - accident_ratio, 
-                          fill = accident - accident_ratio)) + geom_col() + theme(plot.title=element_text(size=30))+
-                                          labs(title = '어느 부서가 사고가 더 잘 발생할까?',
-                                                x = '부서',
-                                                y = '부서별 사고율 - 사내 사고율')# 부서별 사고율
+                      fill = accident - accident_ratio)) + geom_col() + theme(plot.title=element_text(size=30))+
+                      labs(title = '어느 부서가 사고가 더 잘 발생할까?',
+                            x = '부서',
+                            y = '부서별 사고율 - 사내 사고율') 
 
-sales_accident_chart 
-
-
-table(HR$sales)
+sales_accident_chart# 부서별 사고율
 
 
-sales_accident
-
-table(HR$sales)
-
-
-t(table(HR$sales))
-
-B = as.data.frame(A)
